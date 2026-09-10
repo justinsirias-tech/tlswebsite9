3c504660dad622f8b5fa08551a6683a293a6f238
Justin Sirias <justin@thatlaundryshop.com>
2026-09-10 22:33:30 +0700

    feat: Install Google Tag Manager GTM-MKR9HGB

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -35,6 +35,23 @@ export default async function RootLayout({ children, params }) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" precedence="default" />
       </head>
       <body className={inter.className} suppressHydrationWarning>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MKR9HGB"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MKR9HGB');
+          `}
+        </Script>
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-545113254" strategy="afterInteractive" />
         <Script id="google-analytics" strategy="afterInteractive">
           {`
